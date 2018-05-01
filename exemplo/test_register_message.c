1c7fe2ade3457cdd69427e7641bdd33cd771e61e
Unknown <leonardo98ti@gmail.com>
2018-05-01 16:43:58 -0300

    Aceitando texto com até 350 char

--- exemplo/test_register_message.c
+++ exemplo/test_register_message.c
@@ -2,10 +2,19 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <errno.h>
-#include "sys_register_message.h"
+#include "sys_register_info.h"
 
 int main( int argc, char *argv[ ] ){
-	int err = syscall(357, argv[1], argv[2], strlen(argv[2]));
+    int cont;
+    char mensagem[360];
+    
+    bzero(mensagem, 360);
+    for(cont=2; cont < argc; cont++){
+        strcat(mensagem, argv[cont]);
+        strcat(mensagem, " ");
+    }
+    strcat(mensagem, "\n");
+	int err = syscall(357, argv[1], mensagem, strlen(mensagem));
 	if (!err){
 		printf("Arquivo criado com sucesso :) \n");
 	}
